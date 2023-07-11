58b8249750430776dc9506ba328c7ee75c90e2a7
Mike Paulson <mikepaulson@hotmail.com>
2023-07-11 15:50:39 -0400

    Clean up generate-embeddings

--- supabase/functions/generate-embeddings/index.ts
+++ supabase/functions/generate-embeddings/index.ts
@@ -1,7 +1,3 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 import { OpenAI } from "https://deno.land/x/openai/mod.ts";
@@ -12,37 +8,40 @@ serve(async (req) => {
     Deno.env.get('SUPABASE_URL'),
     Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')
   );
-  
   const openAI = new OpenAI(Deno.env.get('OPEN_AI_API_KEY'));
 
+  // Clear the old values
+  await clearQueriesTable(supabaseClient);
+  // Get the new values
   const queryData = await getQueryData(supabaseClient);
 
-  // Clear the old values
-  await supabaseClient
-    .from('queries')
-    .delete()
-    .not('id', 'is', null);
+  for (const data of queryData) {
+    console.log("create embedding: \"" + data.query + "\"");
+    const embedding = await getOpenAIEmbedding(openAI, data.query);
 
-  for (const qd of queryData) {
-    console.log("create embedding: \"" + qd.query + "\"");
-
-    const embeddingsResponse = await openAI.createEmbeddings({
-      model: 'text-embedding-ada-002',
-      input: qd.query,
-    })
-
+    // Add the new record
     await supabaseClient.from('queries').insert({
-      content: qd.query,
-      metadata: qd.metadata,
-      embedding: embeddingsResponse.data[0].embedding
+      content: data.query,
+      metadata: data.metadata,
+      embedding
     });
   }
 
+  console.log("Done.");
   return new Response(
     "Added " + queryData.length + " embeddings.",
     { headers: { "Content-Type": "application/json" } },
   )
 });
+
+async function clearQueriesTable(supabaseClient) {
+  // Need a filter when deleting records, so I have a filter
+  // that just returns everything (id is not null)
+  await supabaseClient
+    .from('queries')
+    .delete()
+    .not('id', 'is', null);
+}
 
 async function getQueryData(supabaseClient) {
   const { data, error } = await supabaseClient
@@ -51,8 +50,8 @@ async function getQueryData(supabaseClient) {
     .download('queries.tsv');
 
   const tsvText = await data.text();
-
   const json = tsv2json(tsvText);
+
   const formatted = json.map(item => {
     return { 
       query: item[0],
@@ -61,4 +60,13 @@ async function getQueryData(supabaseClient) {
   });
 
   return formatted;
+}
+
+async function getOpenAIEmbedding(openAI, input) {
+  const embeddingsResponse = await openAI.createEmbeddings({
+    model: 'text-embedding-ada-002',
+    input
+  });
+
+  return embeddingsResponse.data[0].embedding;
 }
