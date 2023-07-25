511dd3bfc75f03224cd8c5daeab44c9394e01762
otrostianchuk <otrostianchuk@venacorp.com>
2023-07-25 09:53:22 -0400

    fixed bug with querying the same table on multiple levels

--- app/api/query/prompt.ts
+++ app/api/query/prompt.ts
@@ -3,17 +3,18 @@ import {PromptTemplate} from "langchain/prompts";
 import {OpenAI} from "langchain/llms/openai";
 
 
-const prompt = PromptTemplate.fromTemplate(`Given MySQL table A with columns
+const prompt = PromptTemplate.fromTemplate(`Given table with columns
             {columns}
             and rows 
             {rows}.
-            Assume that Value in table A is numeric.In 100 words or less calculate the answer on the question {question}
-            and do not expose any details about table or its structure'`);
+            Assume that rollupValues in table is numeric.In 100 words or less calculate the answer on the question {question}
+            and do not expose any details about table or its structure`);
 
 export default async function getCompletion(query: string, rollupResult: RollupResult): Promise<string> {
     const columns = rollupResult.columns.map((column) => column.name);
     const rows: string[] = rollupResult.rows.map((row: Record<string, any>) => {
-        return columns.slice(0, -1).map((header) => `${row[header]}`).join(",") + "\n";
+        let rowWithValues = columns.map((header) => `${row[header]}`).join(", ");
+        return rowWithValues + "\n";
     });
 
     const formattedPrompt = await prompt.format({
@@ -22,11 +23,12 @@ export default async function getCompletion(query: string, rollupResult: RollupR
         question: query
     });
 
+    console.log(formattedPrompt);
+
     const model = new OpenAI({temperature: 1, openAIApiKey: process.env.OPEN_AI_API_KEY});
 
-    const result = await model.call(
-        formattedPrompt);
+    const result = await model.call(formattedPrompt);
     console.log(result);
 
-    return result;
+    return result.slice(2, result.length - 1);
 }
--- app/api/query/rollup.ts
+++ app/api/query/rollup.ts
@@ -1,5 +1,5 @@
 import sql from './db';
-import {ModelContext, RollupResult} from "@/app/types";
+import {MemberMetadata, ModelContext, RollupResult} from "@/app/types";
 import {TABLE_PER_DIM} from "@/app/api/query/constants";
 
 export default async function rollup(context: ModelContext): Promise<RollupResult> {
@@ -9,17 +9,59 @@ export default async function rollup(context: ModelContext): Promise<RollupResul
     var conditions: string[] = [];
 
     for (const [member, memberMetadata] of Object.entries(context.get())) {
-        var tableDetails = TABLE_PER_DIM[member];
+        const tableDetails = TABLE_PER_DIM[member];
+        const tableName = tableDetails.tableName;
+        const resultingSetOfMembers: string[] = [];
 
-        joins.push(createJoinFromParams(tableDetails.tableName, tableDetails.shortName, tableDetails.foreignKeyColumn));
+        var deepestMemberFound = false;
+        var deepestLevel = -1;
+
+        // Part 1: For every member find the deepest non-null child on the same level, so, the resulting query for dimension won't have more than one level
+        if (memberMetadata.length > 0) {
+            do {
+                const deepestContextLevel = deepestLevel < 0 ? memberMetadata.reduce((max, current) => (current.level > max.level ? current : max), memberMetadata[0]).level : deepestLevel;
+
+                for (const metadata of memberMetadata.filter(metadata => metadata !== undefined)) {
+                    if (metadata.level < deepestContextLevel) {
+                        //fetch the deepest levels for current member and check if on that level they are not null
+                        const deepestMembers = await sql`${sql.unsafe(createQueryToFindDeepestLevel(tableName, deepestContextLevel, metadata))}`;
+
+                        const resultingMemberOnDeepestLevel = deepestMembers[0][`${tableName}_level${deepestContextLevel}`];
+                        if (resultingMemberOnDeepestLevel === null || resultingMemberOnDeepestLevel == 'null') {
+                            deepestMemberFound = false;
+                            deepestLevel = deepestContextLevel - 1;
+                            break;
+                        }
+
+                        deepestMembers.forEach(row => {
+                            !resultingSetOfMembers.includes(row[`${tableName}_level${deepestContextLevel}`]) && resultingSetOfMembers.push(row[`${tableName}_level${deepestContextLevel}`]);
+                        })
+
+                        deepestMemberFound = true;
+                        deepestLevel = deepestContextLevel;
+                    } else {
+                        if (metadata.level == deepestContextLevel) {
+                            !resultingSetOfMembers.includes(metadata.member) && resultingSetOfMembers.push(metadata.member);
+                        }
+
+                        deepestMemberFound = true;
+                        deepestLevel = deepestContextLevel;
+                    }
+                }
+
+            } while (!deepestMemberFound);
+        }
+
+        // Part 2: Start making the whole SQL query from the context
+        joins.push(createJoinFromParams(tableName, tableDetails.shortName, tableDetails.foreignKeyColumn));
 
         if (memberMetadata.length > 0) {
             let memberConditions: Record<string, string[]> = {};
 
-            memberMetadata.filter(metadata => metadata !== undefined).forEach((metadata) => {
-                var columnName = `${tableDetails.tableName}_level${metadata.level}`;
+            resultingSetOfMembers.forEach((metadata) => {
+                var columnName = `${tableName}_level${deepestLevel}`;
 
-                (memberConditions[columnName] ??= []).push(`'${metadata.member}'`);
+                (memberConditions[columnName] ??= []).push(`'${metadata}'`);
                 selects.includes(`${tableDetails.shortName}.${columnName}`) || selects.push(`${tableDetails.shortName}.${columnName}`);
             });
 
@@ -30,6 +72,7 @@ export default async function rollup(context: ModelContext): Promise<RollupResul
             selects.push(tableDetails.shortName + "." + tableDetails.tableName);
         }
     }
+
     var finalQuery = createResultingQuery(selects, joins, conditions);
     console.log(finalQuery);
 
@@ -37,6 +80,12 @@ export default async function rollup(context: ModelContext): Promise<RollupResul
 
     console.log(rollupResult);
     return {columns: rollupResult["columns"], rows: rollupResult};
+}
+
+function createQueryToFindDeepestLevel(tableName: string, deepestContextLevel: number, memberToSearchFor: MemberMetadata): string {
+    return `select ${tableName}_level${deepestContextLevel}
+            from ${tableName}
+            where ${tableName}_level${memberToSearchFor.level} = '${memberToSearchFor.member}'`;
 }
 
 function createJoinFromParams(tableName: string, shortName: string, fk: string): string {
