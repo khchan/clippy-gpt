14faf8178593eeadba2963b070b5b2572ec68fbb
Kevin Chan <kechan@venacorp.com>
2023-07-27 16:06:02 -0400

    update env variable name

--- api/python/index.py
+++ api/python/index.py
@@ -17,7 +17,7 @@ def visualize():
     # exec(script.content)
 
   url: str = os.environ.get("NEXT_PUBLIC_SUPABASE_URL")
-  key: str = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
+  key: str = os.environ.get("NEXT_SUPABASE_SERVICE_ROLE_KEY")
   supabase: Client = create_client(url, key)
   destination = '/tmp/tmp.csv'
   with open(destination, 'wb+') as f:
