3804145a936b04f64e656b03e391a8c06b2eeb40
otrostianchuk <otrostianchuk@venacorp.com>
2023-07-28 01:27:02 -0400

    updated title

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Banner() {
   return (
     <div className="bg-green-700 flex justify-center p-4">
-      <span className="text-white text-bold">ClippyGPT</span>
+      <span className="text-white text-bold">Vena Insights GPT</span>
     </div>
   );
 }
